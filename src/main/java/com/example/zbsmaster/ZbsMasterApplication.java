b84f55e09e44faa320eec586e6e96a2942fcb510
zhangboshi <2858672833@qq.com>
2023-08-13 16:35:17 +0800

    hot fix commit 1

--- src/main/java/com/example/zbsmaster/ZbsMasterApplication.java
+++ src/main/java/com/example/zbsmaster/ZbsMasterApplication.java
@@ -12,6 +12,7 @@ public class ZbsMasterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZbsMasterApplication.class, args);
+        System.out.println("hello git 4");
     }
 
 }
