38a026b2e70258a88a2a17932d109c3fb8c6bfe7
zhangboshi <2858672833@qq.com>
2023-08-13 16:28:10 +0800

    second commit

--- src/main/java/com/example/zbsmaster/ZbsMasterApplication.java
+++ src/main/java/com/example/zbsmaster/ZbsMasterApplication.java
@@ -8,6 +8,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 public class ZbsMasterApplication {
+    //hello
 
     public static void main(String[] args) {
         SpringApplication.run(ZbsMasterApplication.class, args);
